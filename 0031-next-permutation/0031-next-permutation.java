2723be642cdb6af2494ae05e75e55a29c5c694da
Shrijay Talap <shrijay2411@gmail.com>
2026-07-15 16:00:44 +0530

    Time: 0 ms (100%), Space: 44.8 MB (32.82%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,37 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-
+        if (nums == null || nums.length <= 1) return;
+        
         int i = nums.length - 2;
-
-        // Find first smaller number from the end
+        // Step 1: Find the first decreasing element from the right
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
-
-        // If found, swap with next greater number
+        
+        // Step 2: If the pivot was found, find the element to swap with
         if (i >= 0) {
             int j = nums.length - 1;
-
             while (nums[j] <= nums[i]) {
                 j--;
             }
-
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+            swap(nums, i, j);
         }
-
-        // Reverse the remaining part
-        int left = i + 1;
-        int right = nums.length - 1;
-
-        while (left < right) {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-
-            left++;
-            right--;
+        
+        // Step 3: Reverse the descending suffix to get the next lexicographical order
+        reverse(nums, i + 1, nums.length - 1);
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
         }
     }
 }
